9e8064c161dc5d4f8dba496bbec656df00db4764
jakub41 <lemiszewski@gmx.com>
2019-11-21 13:13:25 +0100

    Making up a better structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import SingleBook from './components/SingleBookComponents';
 
 function App() {
   return (
-    <div>
-      <SingleBook />
-    </div>
+    <>
+    <SingleBook />
+    </>
   );
 }
 
--- src/components/SingleBookComponents.jsx
+++ src/components/SingleBookComponents.jsx
@@ -1,15 +1,31 @@
-import React, { Component } from 'react'
-import { Col, Card, CardImg, CardTitle, CardBody, CardSubtitle, Button } from "reactstrap";
-import Book from '../Books/scifi.json';
+import React, {Component} from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  CardImg,
+  CardTitle,
+  CardBody,
+  CardSubtitle,
+  Button,
+} from "reactstrap";
+import Book from "../Books/scifi.json";
 
 export default class SingleBookComponents extends Component {
-    render() {
-        let book = Book[0];
-        console.log(book)
-        return (
-            <div>
-                
-            </div>
-        )
-    }
+  render() {
+    let book = Book[0];
+    console.log(book);
+    return (
+      <>
+        <Container>
+            <Row>
+                <Col md="4">
+                    
+                </Col>
+            </Row>
+        </Container>
+      </>
+    );
+  }
 }
