9d9f6c6ad04f1aee0c560d25eff63038f90ce148
jakub41 <lemiszewski@gmx.com>
2019-11-22 00:20:03 +0100

    Added Api set up

---
+++ src/Apis/BasicApi.jsx
@@ -0,0 +1,20 @@
+class BasicApi {
+    constructor() {
+
+        this.token = btoa("user25" + ":" + "gX7HF4hYaYyJAzpt");
+        this.domain = 'strive-school-testing-apis.herokuapp.com/api'
+
+        this.protocol = 'https';
+        this.credentials = {
+            method: 'GET',
+            headers: new Headers({
+                'Authorization': 'Basic ' + this.token,
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }),
+        };
+        this.url = `${this.protocol}://${this.domain}`;
+    }
+}
+
+export default BasicApi;
---
+++ src/Apis/CommentApi.jsx
@@ -0,0 +1,64 @@
+import BasicApi from "./BasicApi";
+
+class CommentApi extends BasicApi {
+    getAllComments = async (asin) => {
+        try {
+            const uri = `${this.url}/comments/${asin}`;
+            const repsonse = await fetch(uri, this.credentials);
+            const json = await repsonse.json();
+            return { success: true, data: json };
+        } catch {
+            return {
+                success: false,
+                result: [],
+                message: 'There is an issue to get data from server. Please try again later.',
+            };
+        }
+    }
+    addComment = async (body) => {
+        try {
+            const uri = `${this.url}/comments`;
+            this.credentials.method = "POST";
+            this.credentials.body = JSON.stringify(body);
+            const repsonse = await fetch(uri, this.credentials);
+            const json = await repsonse.json();
+            return { success: true, data: json };
+        } catch {
+            return {
+                success: false,
+                message: 'There is an issue to get data from server. Please try again later.',
+            };
+        }
+    }
+    updateComment = async (id, body) => {
+        try {
+            const uri = `${this.url}/comments/${id}`;
+            this.credentials.method = "PUT";
+            this.credentials.body = JSON.stringify(body);
+            const repsonse = await fetch(uri, this.credentials);
+            const json = await repsonse.json();
+            return { success: true, data: json };
+        } catch {
+            return {
+                success: false,
+                message: 'There is an issue to get data from server. Please try again later.',
+            };
+        }
+    }
+
+    deleteComment = async (id) => {
+        try {
+            const uri = `${this.url}/comments/${id}`;
+            this.credentials.method = "DELETE";
+            const repsonse = await fetch(uri, this.credentials);
+            const json = await repsonse.json();
+            return { success: true, data: json };
+        } catch {
+            return {
+                success: false,
+                message: 'There is an issue to get data from server. Please try again later.',
+            };
+        }
+    }
+}
+export default CommentApi;
